3818ea1d917f94e0f59c1767611d921f1894c3fb
ies.cho <ies.cho@sec.com>
2025-07-23 15:39:52 +0900

    [refactor] TC3 getLengthScore

--- StringCompare/StringCompare.cpp
+++ StringCompare/StringCompare.cpp
@@ -10,14 +10,31 @@ public:
             return 60;
         }
         else {
-            float long_length = (str1.length() > str2.length()) ? str1.length() : str2.length();
-            float short_length = (str1.length() < str2.length()) ? str1.length() : str2.length();
+            float long_length = getLongStrLen(str1, str2);
+            float short_length = getShortStrLen(str1, str2);
 
-            if (long_length >= short_length * 2) {
-                return 0;
+            if (!isZeroScore(long_length, short_length)) {
+                return getSubScore(long_length, short_length);
             }
-            return (1.0f - ((long_length - short_length) / short_length)) * 60.0f;
         }
         return 0;
+    }
+
+private:
+    float getSubScore(float long_length, float short_length)
+    {
+        return (1.0f - ((long_length - short_length) / short_length)) * 60.0f;
+    }
+    bool isZeroScore(float long_length, float short_length)
+    {
+        return long_length >= short_length * 2;
+    }
+    size_t getShortStrLen(std::string& str1, std::string& str2)
+    {
+        return (str1.length() < str2.length()) ? str1.length() : str2.length();
+    }
+    size_t getLongStrLen(std::string& str1, std::string& str2)
+    {
+        return (str1.length() > str2.length()) ? str1.length() : str2.length();
     }
 };

7325bfabc139cdde4547ea9ab811b761a170ff6f
ies.cho <ies.cho@sec.com>
2025-07-23 16:01:24 +0900

    [refactor] Remove function

--- StringCompare/StringCompare.cpp
+++ StringCompare/StringCompare.cpp
@@ -6,21 +6,12 @@ using namespace std;
 class StringCompare {
 public:
     int getLengthScore(string str1, string str2) {
-        if (str1.length() == str2.length()) {
-            return 60;
-        }
         return getSubScore(getLongStrLen(str1, str2), getShortStrLen(str1, str2));
     }
 
 private:
-    bool isZeroScore(float long_length, float short_length)
-    {
-        return (long_length >= short_length * 2) ? true : false;
-    }
-
     float getSubScore(float long_length, float short_length)
     {
-        if (isZeroScore(long_length, short_length)) return 0;
         return (1.0f - ((long_length - short_length) / short_length)) * 60.0f;
     }
 

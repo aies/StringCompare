6f8b29eac8c75946179df193df7a25052d2e8205
ies.cho <ies.cho@sec.com>
2025-07-23 15:48:07 +0900

    [refactor] TC4 getLengthScore

--- StringCompare/StringCompare.cpp
+++ StringCompare/StringCompare.cpp
@@ -9,30 +9,26 @@ public:
         if (str1.length() == str2.length()) {
             return 60;
         }
-        else {
-            float long_length = getLongStrLen(str1, str2);
-            float short_length = getShortStrLen(str1, str2);
-
-            if (!isZeroScore(long_length, short_length)) {
-                return getSubScore(long_length, short_length);
-            }
-        }
-        return 0;
+        return getSubScore(getLongStrLen(str1, str2), getShortStrLen(str1, str2));
     }
 
 private:
-    float getSubScore(float long_length, float short_length)
-    {
-        return (1.0f - ((long_length - short_length) / short_length)) * 60.0f;
-    }
     bool isZeroScore(float long_length, float short_length)
     {
-        return long_length >= short_length * 2;
+        return (long_length >= short_length * 2) ? true : false;
     }
+
+    float getSubScore(float long_length, float short_length)
+    {
+        if (isZeroScore(long_length, short_length)) return 0;
+        return (1.0f - ((long_length - short_length) / short_length)) * 60.0f;
+    }
+
     size_t getShortStrLen(std::string& str1, std::string& str2)
     {
         return (str1.length() < str2.length()) ? str1.length() : str2.length();
     }
+
     size_t getLongStrLen(std::string& str1, std::string& str2)
     {
         return (str1.length() > str2.length()) ? str1.length() : str2.length();

11494736e0b15389b7d9cf862c2d4eabd11bdddf
ies.cho <ies.cho@sec.com>
2025-07-23 15:34:47 +0900

    [feature] TC3 getLengthScore

--- StringCompare/StringCompare.cpp
+++ StringCompare/StringCompare.cpp
@@ -6,9 +6,18 @@ using namespace std;
 class StringCompare {
 public:
     int getLengthScore(string str1, string str2) {
-        if (str1.length() == str2.length())
+        if (str1.length() == str2.length()) {
             return 60;
-        else
-            return 0;
+        }
+        else {
+            float long_length = (str1.length() > str2.length()) ? str1.length() : str2.length();
+            float short_length = (str1.length() < str2.length()) ? str1.length() : str2.length();
+
+            if (long_length >= short_length * 2) {
+                return 0;
+            }
+            return (1.0f - ((long_length - short_length) / short_length)) * 60.0f;
+        }
+        return 0;
     }
 };
--- StringCompare/StringCompare_test.cpp
+++ StringCompare/StringCompare_test.cpp
@@ -15,6 +15,10 @@ TEST_F(StringCMPFixture, CheckStringLengthScore2) {
     EXPECT_EQ(sc.getLengthScore("A", "BB"), 0);
 }
 
+TEST_F(StringCMPFixture, CheckStringLengthScore3) {
+    EXPECT_EQ(sc.getLengthScore("AAABB", "BAA"), 19);
+}
+
 int main() {
     ::testing::InitGoogleMock();
     return RUN_ALL_TESTS();

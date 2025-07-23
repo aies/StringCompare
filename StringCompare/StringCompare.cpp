4b8272779688b0118373df0bbab404fc0b87503e
ies.cho <ies.cho@sec.com>
2025-07-23 15:26:15 +0900

    [refactor] TC2 getLengthScore

--- StringCompare/StringCompare.cpp
+++ StringCompare/StringCompare.cpp
@@ -6,7 +6,6 @@ using namespace std;
 class StringCompare {
 public:
     int getLengthScore(string str1, string str2) {
-
         if (str1.length() == str2.length())
             return 60;
         else
--- StringCompare/StringCompare_test.cpp
+++ StringCompare/StringCompare_test.cpp
@@ -1,17 +1,20 @@
 #include "gmock/gmock.h"
 #include "StringCompare.cpp"
 
-TEST(tc, CheckStringLengthScore) {
+using namespace testing;
+class StringCMPFixture : public Test {
+public:
     StringCompare sc;
+};
 
-    EXPECT_EQ(sc.getLengthScore("ASD","DSA"), 60);
+TEST_F(StringCMPFixture, CheckStringLengthScore) {
+    EXPECT_EQ(sc.getLengthScore("ASD", "DSA"), 60);
 }
 
-TEST(tc, CheckStringLengthScore2) {
-    StringCompare sc;
-
+TEST_F(StringCMPFixture, CheckStringLengthScore2) {
     EXPECT_EQ(sc.getLengthScore("A", "BB"), 0);
 }
+
 int main() {
     ::testing::InitGoogleMock();
     return RUN_ALL_TESTS();
